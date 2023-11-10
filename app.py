24e5442aa2839205530fba6bdcdd3c6dca0636c1
admin <admin@example.com>
2023-11-10 07:13:59 +0000

    adding app ports and new endpoint

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
+
+
+@app.route("/ping")
+def ping():
+    return "Pong"
